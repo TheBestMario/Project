228bdb1e7b7991211b45d00255b2f2101748cbba
TheBestMario <mariogeorgiev02@gmail.com>
2025-02-13 15:04:08 +0000

    update content after refactoring calendarView

--- src/main/java/org/example/projectcalendar/controllers/Content.java
+++ src/main/java/org/example/projectcalendar/controllers/Content.java
@@ -1,34 +1,29 @@
 package org.example.projectcalendar.controllers;
 
-import com.calendarfx.view.CalendarView;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.layout.AnchorPane;
-import org.example.projectcalendar.Controller;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.example.projectcalendar.Controller;
+
+import com.calendarfx.view.CalendarView;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+
 public class Content extends Controller implements Initializable {
 
-
+    @FXML
     public AnchorPane anchorPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        CalendarView calendarView = new CalendarView();
-        calendarView.showMonthPage();
-        calendarView.setEnableTimeZoneSupport(true);
-        anchorPane.getChildren().add(calendarView);
-        AnchorPane.setTopAnchor(calendarView, 0.0);
-        AnchorPane.setBottomAnchor(calendarView, 0.0);
-        AnchorPane.setRightAnchor(calendarView, 0.0);
-        AnchorPane.setLeftAnchor(calendarView, 0.0);
-
-
+        showCalendarView();
     }
+
     public void setView(String fxmlPath, Controller controller) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
@@ -43,6 +38,17 @@ public class Content extends Controller implements Initializable {
             e.printStackTrace();
         }
     }
+
+    public void showCalendarView() {
+        CalendarViewController calendarController = new CalendarViewController();
+        CalendarView calendarView = calendarController.createCalendarView();
+        anchorPane.getChildren().setAll(calendarView);
+        AnchorPane.setTopAnchor(calendarView, 0.0);
+        AnchorPane.setBottomAnchor(calendarView, 0.0);
+        AnchorPane.setLeftAnchor(calendarView, 0.0);
+        AnchorPane.setRightAnchor(calendarView, 0.0);
+    }
+
     public <T extends Node> void setView(T view, Controller controller) {
         anchorPane.getChildren().setAll(view);
         AnchorPane.setTopAnchor(view, 0.0);
